fd03c5d82c712fdf0f7a13f16fa51e22d0040395
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-12 20:07:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function BoardMember(name, homeState, training) {
   this.veto = function(){
     return(`No, I must disagree`);
   }
+  this.approve = function(){
+    return(`You can do that!`);
+  }
 }
 
 //
