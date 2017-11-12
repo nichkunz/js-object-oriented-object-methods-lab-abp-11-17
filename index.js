367a96976f79217716fcdac8af0ab41e62a07e4d
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-12 20:04:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
 function BoardMember(name, homeState, training) {
-  
+  this.name = name
+  this.homeState = homeState
+  this.training = training
 }
 
-// 
+//
 // + `approve` — returns `You can do that!`
-// 
+//
 // + `doCharity` — returns `I like to help people.`
-// 
+//
 // + `releasePressStatement` — returns `You will see great things from Scuber.`
-// 
+//
 // + `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
